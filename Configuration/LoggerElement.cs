14c8d4725e5bbc64a9d2f6d0e92ffd19766ce2f7
dotyc <dotyc@civicinc.com>
2016-03-26 13:26:28 -0500

    unify all logging configuration items

--- Configuration/LoggerElement.cs
+++ Configuration/LoggerElement.cs
@@ -17,29 +17,49 @@ namespace Civic.Core.Logging.Configuration
 
         public string Type { get; set; }
 
-        public Dictionary<string, string> Attributes { get; private set; }
+        public Dictionary<string, string> Attributes { get; set; }
+
+        public List<string> AppliesTo { get; set; }
+
+        public List<string> FilterBy { get; set; }
+
+        public int RescanTime { get; set; }
+
+        public ILogWriter Writer { get; set; }
 
         public static LoggerElement Create(INamedElement configElement)
         {
             if (string.IsNullOrEmpty(configElement.Name) || !configElement.Attributes.ContainsKey(Constants.CONFIG_TYPE_PROP))
-                throw new ConfigurationErrorsException("loggerElement element must contain a name and a type for the log writer");
+                throw new ConfigurationErrorsException("projectConfig element must contain a name and a type for the log reader/writer");
 
-            string assembly = typeof (Logger).Assembly.FullName;
+            string assembly = typeof(Logger).Assembly.FullName;
             if (configElement.Attributes.ContainsKey(Constants.CONFIG_ASSEMBLY_PROP))
                 assembly = configElement.Attributes[Constants.CONFIG_ASSEMBLY_PROP];
 
             var config = new LoggerElement
-                {
-                    Name = configElement.Name,
-                    Assembly = assembly,
-                    Type = configElement.Attributes[Constants.CONFIG_TYPE_PROP],
-                    Attributes = configElement.Attributes
-                };
+            {
+                Name = configElement.Name,
+                Assembly = assembly,
+                Type = configElement.Attributes[Constants.CONFIG_TYPE_PROP],
+                Attributes = configElement.Attributes
+            };
 
             if (config.Attributes == null) config.Attributes = new Dictionary<string, string>();
 
+            config.RescanTime = config.Attributes.ContainsKey(Constants.CONFIG_CHECKFORENTRIESTIME_PROP)
+                ? int.Parse(config.Attributes[Constants.CONFIG_CHECKFORENTRIESTIME_PROP])
+                : LoggerSection.Current.DefaultCheckForEntriesTime;
+
+            config.FilterBy = configElement.Attributes.ContainsKey(Constants.CONFIG_FILTERBY_PROP) ?
+                        new List<string>(configElement.Attributes[Constants.CONFIG_FILTERBY_PROP].Split(','))
+                      : new List<string>();
+
+            config.AppliesTo = configElement.Attributes.ContainsKey(Constants.CONFIG_APPLIESTO_PROP) ?
+                        new List<string>(configElement.Attributes[Constants.CONFIG_APPLIESTO_PROP].Split(','))
+                      : new List<string>();
+
             return config;
         }
-    
+
     }
 }
