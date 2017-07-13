60a71a827ea1defa8a2d5cdc18e6eea36fa441fa
Chris Doty <dotyc@civicinc.com>
2017-07-13 08:48:34 -0500

    was not logging sql when debugging

--- Civic.Core.Logging/Logger.cs
+++ Civic.Core.Logging/Logger.cs
@@ -257,7 +257,7 @@ namespace Civic.Core.Logging
         public static bool LogTrace(LoggingBoundaries boundary, params object[] parameterValues)
         {
             if (_config == null) Init();
-            if (!IsTraceOn || Debugger.IsAttached) return false;
+            if (!IsTraceOn) return false;
             return Log(LogMessage.LogTrace(boundary, parameterValues));
         }
 
