65987f8beec7cfc21d8b85170c3e10574fc00908
Chris Doty <dotyc@civicinc.com>
2019-01-13 19:54:22 -0600

    ...

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.27.0.0")]
 [assembly: AssemblyFileVersion("1.27.5.3812")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2018 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2019 Civic Engineering & IT")]
